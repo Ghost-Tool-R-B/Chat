030135d439db1b632528306dbc6e297419e965ff
Tranquilty <benchenapa1@googlemail.com>
2015-12-24 14:33:48 -0800

    Base Update

---
+++ Client/Chat.Designer.cs
@@ -0,0 +1,198 @@
+﻿namespace Client
+{
+    partial class Chat
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Chat));
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMsgs = new System.Windows.Forms.TextBox();
+            this.txtSend = new System.Windows.Forms.TextBox();
+            this.lstClients = new System.Windows.Forms.ListBox();
+            this.formAssistant1 = new DevExpress.XtraBars.FormAssistant();
+            this.btnSend = new DevExpress.XtraEditors.SimpleButton();
+            this.simpleButton1 = new DevExpress.XtraEditors.SimpleButton();
+            this.defaultLookAndFeel1 = new DevExpress.LookAndFeel.DefaultLookAndFeel();
+            this.textEdit1 = new DevExpress.XtraEditors.TextEdit();
+            this.textEdit2 = new DevExpress.XtraEditors.TextEdit();
+            this.simpleButton2 = new DevExpress.XtraEditors.SimpleButton();
+            this.labelControl1 = new DevExpress.XtraEditors.LabelControl();
+            this.labelControl2 = new DevExpress.XtraEditors.LabelControl();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 115);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(63, 13);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "User Name:";
+            // 
+            // txtMsgs
+            // 
+            this.txtMsgs.Location = new System.Drawing.Point(12, 141);
+            this.txtMsgs.Multiline = true;
+            this.txtMsgs.Name = "txtMsgs";
+            this.txtMsgs.Size = new System.Drawing.Size(333, 199);
+            this.txtMsgs.TabIndex = 11;
+            // 
+            // txtSend
+            // 
+            this.txtSend.Location = new System.Drawing.Point(95, 346);
+            this.txtSend.Multiline = true;
+            this.txtSend.Name = "txtSend";
+            this.txtSend.Size = new System.Drawing.Size(376, 51);
+            this.txtSend.TabIndex = 10;
+            this.txtSend.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtSend_KeyPress);
+            // 
+            // lstClients
+            // 
+            this.lstClients.FormattingEnabled = true;
+            this.lstClients.Location = new System.Drawing.Point(351, 141);
+            this.lstClients.Name = "lstClients";
+            this.lstClients.Size = new System.Drawing.Size(120, 199);
+            this.lstClients.TabIndex = 9;
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(15, 346);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(75, 51);
+            this.btnSend.TabIndex = 15;
+            this.btnSend.Text = "Send";
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // simpleButton1
+            // 
+            this.simpleButton1.Location = new System.Drawing.Point(351, 115);
+            this.simpleButton1.Name = "simpleButton1";
+            this.simpleButton1.Size = new System.Drawing.Size(120, 23);
+            this.simpleButton1.TabIndex = 14;
+            this.simpleButton1.Text = "Connect";
+            this.simpleButton1.Click += new System.EventHandler(this.simpleButton1_Click);
+            // 
+            // defaultLookAndFeel1
+            // 
+            this.defaultLookAndFeel1.LookAndFeel.SkinName = "Visual Studio 2013 Dark";
+            // 
+            // textEdit1
+            // 
+            this.textEdit1.Location = new System.Drawing.Point(68, 2);
+            this.textEdit1.Name = "textEdit1";
+            this.textEdit1.Size = new System.Drawing.Size(100, 20);
+            this.textEdit1.TabIndex = 16;
+            // 
+            // textEdit2
+            // 
+            this.textEdit2.Location = new System.Drawing.Point(68, 28);
+            this.textEdit2.Name = "textEdit2";
+            this.textEdit2.Size = new System.Drawing.Size(100, 20);
+            this.textEdit2.TabIndex = 17;
+            // 
+            // simpleButton2
+            // 
+            this.simpleButton2.Location = new System.Drawing.Point(93, 54);
+            this.simpleButton2.Name = "simpleButton2";
+            this.simpleButton2.Size = new System.Drawing.Size(75, 23);
+            this.simpleButton2.TabIndex = 18;
+            this.simpleButton2.Text = "Sign In";
+            this.simpleButton2.Click += new System.EventHandler(this.simpleButton2_Click);
+            // 
+            // labelControl1
+            // 
+            this.labelControl1.Location = new System.Drawing.Point(12, 5);
+            this.labelControl1.Name = "labelControl1";
+            this.labelControl1.Size = new System.Drawing.Size(52, 13);
+            this.labelControl1.TabIndex = 19;
+            this.labelControl1.Text = "Username:";
+            // 
+            // labelControl2
+            // 
+            this.labelControl2.Location = new System.Drawing.Point(12, 31);
+            this.labelControl2.Name = "labelControl2";
+            this.labelControl2.Size = new System.Drawing.Size(50, 13);
+            this.labelControl2.TabIndex = 20;
+            this.labelControl2.Text = "Password:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(81, 115);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "....";
+            // 
+            // Chat
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(477, 405);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.labelControl2);
+            this.Controls.Add(this.labelControl1);
+            this.Controls.Add(this.simpleButton2);
+            this.Controls.Add(this.textEdit2);
+            this.Controls.Add(this.textEdit1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtMsgs);
+            this.Controls.Add(this.txtSend);
+            this.Controls.Add(this.lstClients);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.simpleButton1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Chat";
+            this.Text = "Ghost Chat";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form2_FormClosing);
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit1.Properties)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.textEdit2.Properties)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMsgs;
+        private System.Windows.Forms.TextBox txtSend;
+        private System.Windows.Forms.ListBox lstClients;
+        private DevExpress.XtraBars.FormAssistant formAssistant1;
+        private DevExpress.XtraEditors.SimpleButton btnSend;
+        private DevExpress.XtraEditors.SimpleButton simpleButton1;
+        private DevExpress.LookAndFeel.DefaultLookAndFeel defaultLookAndFeel1;
+        private DevExpress.XtraEditors.TextEdit textEdit1;
+        private DevExpress.XtraEditors.TextEdit textEdit2;
+        private DevExpress.XtraEditors.SimpleButton simpleButton2;
+        private DevExpress.XtraEditors.LabelControl labelControl1;
+        private DevExpress.XtraEditors.LabelControl labelControl2;
+        private System.Windows.Forms.Label label2;
+    }
+}
---
+++ Client/Chat.cs
@@ -0,0 +1,124 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.ServiceModel;
+using Client.ChatService;
+using MySql.Data.MySqlClient;
+using MySql.Data;
+using DevExpress.XtraEditors;
+
+namespace Client
+{
+    public partial class Chat : DevExpress.XtraEditors.XtraForm
+    {
+        ReceiveClient rc = null;
+        string myName;
+        private MySqlConnection connection;
+        string USER = Properties.Settings.Default.Username;
+        public Chat()
+        {
+            simpleButton1.Enabled = false;
+            string connectionString;
+            connectionString = "Server=64.94.238.79; database=ghost; Uid=Ryan; Pwd=COREi7;";
+            connection = new MySqlConnection(connectionString);
+            InitializeComponent();
+            this.FormClosing += new FormClosingEventHandler(Form2_FormClosing);
+            this.txtSend.KeyPress += new KeyPressEventHandler(txtSend_KeyPress);
+        }
+        void rc_ReceiveMsg(string sender, string msg)
+        {
+            if (msg.Length > 0)
+                txtMsgs.Text += Environment.NewLine + sender + ">" + msg;
+        }
+
+        void rc_NewNames(object sender, List<string> names)
+        {
+            lstClients.Items.Clear();
+            foreach (string name in names)
+            {
+                if (name != myName)
+                    lstClients.Items.Add(name);
+            }
+        }
+        private void simpleButton1_Click(object sender, EventArgs e)
+        {
+            if (label2.Text.Length > 0)
+            {
+                txtMsgs.Enabled = true;
+                txtSend.Enabled = true;
+                btnSend.Enabled = true;
+
+                myName = label2.Text.Trim();
+
+                rc = new ReceiveClient();
+                XtraMessageBox.Show("Connected To Chat");
+                rc.Start(rc, myName);
+
+                rc.NewNames += new GotNames(rc_NewNames);
+                rc.ReceiveMsg += new ReceviedMessage(rc_ReceiveMsg);
+            }
+        }
+
+        private void SendMessage()
+        {
+            if (lstClients.Items.Count != 0)
+            {
+                txtMsgs.Text += Environment.NewLine + myName + ">" + txtSend.Text;
+                if (lstClients.SelectedItems.Count == 0)
+                    rc.SendMessage(txtSend.Text, myName, lstClients.Items[0].ToString());
+                else
+                    if (!string.IsNullOrEmpty(lstClients.SelectedItem.ToString()))
+                    rc.SendMessage(txtSend.Text, myName, lstClients.SelectedItem.ToString());
+
+                txtSend.Clear();
+            }
+        }
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            SendMessage();
+        }
+
+        private void txtSend_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            int keyValue = (int)e.KeyChar;
+
+            if (keyValue == 13)
+                SendMessage();
+        }
+
+        private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            rc.Stop(myName);
+            this.Hide();
+            e.Cancel = true;
+        }
+
+        private void simpleButton2_Click(object sender, EventArgs e)
+        {
+            string LoginQ = "Select * from ghost.clients WHERE Username=@par1 AND Password=@par2;";
+            MySqlCommand LoginCMD = new MySqlCommand(LoginQ, connection);
+            LoginCMD.Parameters.AddWithValue("@par1", this.textEdit1.Text);
+            LoginCMD.Parameters.AddWithValue("@par2", textEdit2.Text);
+            try
+            {
+                connection.Open();
+                LoginCMD.ExecuteNonQuery();
+                XtraMessageBox.Show("Welcome - " + textEdit1.Text);
+                USER = textEdit1.Text;
+                Properties.Settings.Default.Save();
+                label2.Text = USER;
+                simpleButton1.Enabled = true;
+                simpleButton2.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                XtraMessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
